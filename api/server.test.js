e558f7a9a637ad303060c2170f3e589a1e6e2c11
PVigar88 <Pvigar@gmail.com>
2021-08-15 16:43:14 -0400

    wrote test conditions

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,37 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+
+const request = require("supertest");
+const server = require("./server");
+const db = require("../data/dbConfig");
+const User = require("./users/users-model");
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
+
+test("sanity", () => {
+  expect(true).toBe(true);
+});
+
+describe("[POST] /auth/register", () => {
+  it.skip("adds a user and returns it", async () => {
+    //test 1
+  });
+  it.skip("returns error if username is already taken", async () => {
+    //test 2
+  });
+});
+
+describe("[POST] /auth/login", () => {
+  it.skip("on successful login returns username and token", async () => {
+    //test 1
+  });
+  it.skip("returns error when wrong credentials are used", async () => {
+    //test 2
+  });
+});
