2770ca199bc6c7d8e7e2f18086bb08df89a4671e
PVigar88 <Pvigar@gmail.com>
2021-08-15 22:05:07 -0400

    added error handler

--- api/middleware/input-validation-middleware.js
+++ api/middleware/input-validation-middleware.js
@@ -10,13 +10,15 @@ const checkAuthPayload = (req, res, next) => {
 
 const checkUsernameAvailable = async (req, res, next) => {
   const { username } = req.body;
-  await User.findBy({ username }).then((user) => {
-    if (user.username) {
-      res.json({ message: "username taken" });
-    } else {
-      next();
-    }
-  });
+  await User.findBy({ username })
+    .then((user) => {
+      if (user.username) {
+        res.json({ message: "username taken" });
+      } else {
+        next();
+      }
+    })
+    .catch({ message: "username taken" });
 };
 
 module.exports = { checkAuthPayload, checkUsernameAvailable };
