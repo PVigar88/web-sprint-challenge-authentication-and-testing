fedeb990db34d9cde1759af52dc1bfa04798e878
PVigar88 <Pvigar@gmail.com>
2021-08-15 21:25:58 -0400

    changed how middleware is delivering errors

--- api/middleware/input-validation-middleware.js
+++ api/middleware/input-validation-middleware.js
@@ -2,7 +2,7 @@ const User = require("../users/users-model");
 
 const checkAuthPayload = (req, res, next) => {
   if (!req.body.username || !req.body.password) {
-    next({ status: 422, message: "username and password required" });
+    res.json({ message: "username and password required" });
   } else {
     next();
   }
@@ -12,7 +12,7 @@ const checkUsernameAvailable = async (req, res, next) => {
   const { username } = req.body;
   await User.findBy({ username }).then((user) => {
     if (user.username) {
-      next({ status: 422, message: "username taken" });
+      res.json({ message: "username taken" });
     } else {
       next();
     }
